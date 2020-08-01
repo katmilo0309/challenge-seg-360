7f43509547f94711e86855a007db591ec56b684a
katmilo0309 <katmilo0309@gmail.com>
2020-08-01 14:06:30 -0500

    first commit to develop

---
+++ src/main/java/com/mercadolibre/challenge/api/ChallengeSeg360Application.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.challenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSeg360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSeg360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/challenge/api/ChallengeSeg360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSeg360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
